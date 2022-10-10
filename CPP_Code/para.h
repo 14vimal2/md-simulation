e4a7787ca204861fd0a72273af8d542aa8b92533
14vimal2 <vimal@ek-pahal.in>
2022-10-10 15:31:27 +0530

    namespaces are created in para.h

--- CPP_Code/para.h
+++ CPP_Code/para.h
@@ -4,6 +4,7 @@
 using namespace std;
 
 
+namespace para {
 const uint32_t N = 9; // Number of particles
 const float T = 0.306; // Temperature of particles i.e used to set velocities
 const float dt = 0.005; // time-step of simulation
@@ -11,14 +12,21 @@ const float tf = 100; // total time of simulation
 const uint8_t d = 2; // dimension of simulation
 const float rc = 2.5; // cut-off radius of Lennard-Jones Potential
 const float rho = 0.00304; // density of particles
+}
 
+
+namespace file_config{
 // directory used to store and input data
 const string dir_name = "sim_data";
 const string positions_input_file = "postions_N1000_d2_rho0.00304_sqaregrid.dat";
 const string velocities_input_file = "velocities_N1000_d2_T0.306.dat";
+}
+
+
 
 //-----------Not to edit------------
-
+namespace para {
 float L = pow(N / rho, 1.0 / d);                       // size of box
 float PE_cut = 4 * (1 / pow(rc, 12) - 1 / pow(rc, 6)); // lj potential at rc
 int N_times_d = d * N;
+}
--- test.cpp
+++ test.cpp
@@ -4,8 +4,8 @@
 
 int main()
 {
-    int sn = ceil(pow(N, 1.0 / d));
-    cout << positions_input_file << endl;
+    int sn = ceil(pow(para::N, 1.0 / para::d));
+    cout << file_config::positions_input_file << endl;
     // for (int i = 0; i < N; i++)
     // {
     //     for (int j = 0; j < d; j++)
@@ -15,25 +15,25 @@ int main()
     // }
     // cout << endl;
 
-    float *arr = new float[N_times_d];
-    for (int i = 0; i < N_times_d; i++)
+    float *arr = new float[para::N_times_d];
+    for (int i = 0; i < para::N_times_d; i++)
     {
-        *(arr+i) = (((i / d) / (int)(pow(sn, i % d))) % sn + 0.5) * L / sn;
+        *(arr+i) = (((i / para::d) / (int)(pow(sn, i % para::d))) % sn + 0.5) * para::L / sn;
     }
 
     ofstream os("CPP_Code/inputpositions.dat", ios::binary);
-    os.write(reinterpret_cast<char*> (arr), N_times_d*sizeof(float));
+    os.write(reinterpret_cast<char*> (arr), para::N_times_d*sizeof(float));
     os.close();
 
-    for (int i = 0; i < N_times_d; i++)
+    for (int i = 0; i < para::N_times_d; i++)
     {
         *(arr+i) = 0;
     }
 
     ifstream is("CPP_Code/inputpositions.dat", ios::binary);
-    is.read(reinterpret_cast<char*>(arr), N_times_d*sizeof(float));
+    is.read(reinterpret_cast<char*>(arr), para::N_times_d*sizeof(float));
 
-    for (int i = 0; i < N_times_d; i++)
+    for (int i = 0; i < para::N_times_d; i++)
     {
         cout << *(arr+i) << " ";
     }
@@ -41,5 +41,5 @@ int main()
     
 
     cout << endl;
-    printf("%f", L);
+    printf("%f", para::L);
 }
